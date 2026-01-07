dc8ad23c033c5425127d37dad61fc36db482c1d3
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-07 13:35:52 +0530

    feat: Add Error Handler

--- application/weather/src/App.jsx
+++ application/weather/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import InputCity from "./Components/InputCity";
 import Header from "./Components/Header";
-import ShowWeather from "./Components/ShowWeather"
 import "./styles.css";
+import ShowWeather from "./Components/ShowWeather"
 
 export default function App() {
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState("");
   const [inputCity, setInputCity] = useState("Seattle");
   const [cityName, setCityName] = useState("Seattle");
   const [error, setError] = useState(false);
@@ -14,7 +14,6 @@ export default function App() {
   const inputHandler = (e) => {
     setInputCity(e.target.value);
   };
-
   //  Search button
   const submitHandler = (e) => {
     e.preventDefault();
@@ -23,11 +22,12 @@ export default function App() {
   };
 
   //  Weather API
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={{api_key_weather_app}}`;
-
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b81a143c479a1155fa90ac077e36acd3`;
 
   //  Fetching weather data
   async function fetchData(URL) {
+
+
     const response = await fetch(URL);
     const data = await response.json();
     if (data.cod === "404") {
@@ -42,16 +42,20 @@ export default function App() {
     fetchData(URL);
   }, [URL]);
 
+
   return (
     <div>
       <Header />
-
       <InputCity
         city={inputCity}
         onInputHandler={inputHandler}
         onSubmitHandler={submitHandler}
       />
-      <ShowWeather data={weatherData}/>
+      {error ? (
+        <h3 className="error">No data found :( </h3>
+      ) : (
+          <ShowWeather data={weatherData} />
+        )}
     </div>
   );
 }
