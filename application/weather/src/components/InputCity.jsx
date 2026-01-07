0f57f34c0c5c310f1810c6a7945dc5871fc002c2
itsjayeshp <jayeshpatidar1310@gmail.com>
2026-01-07 13:38:03 +0530

    feat: Add `InputCity` component and initial global styling for the weather application.

--- application/weather/src/components/InputCity.jsx
+++ application/weather/src/components/InputCity.jsx
@@ -1,13 +1,13 @@
 const InputCity = ({ inputCity, onInputHandler, onSubmitHandler }) => {
   return (
-    <form onSubmit={onSubmitHandler} className="input-container">
+    <form onSubmit={onSubmitHandler} className="input">
       <input
         type="text"
         value={inputCity}
         onChange={onInputHandler}
         placeholder="Enter city name"
       />
-      <button type="submit">Search</button>
+      <button type="submit" className="input_btn">Search</button>
     </form>
   );
 };
